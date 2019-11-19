b2bb0bcc28b6237e8f14f024176c234a03c561db
SuryaprakashM <starsurya96@gmail.com>
2019-11-19 14:59:33 +0530

    Add files via upload

---
+++ Runner2.java
@@ -0,0 +1,56 @@
+package com.cts.peg.bd.streams;
+
+import java.util.Arrays;
+import java.util.Date;
+import scala.Tuple2;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Column;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoder;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.DateType;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+import org.apache.spark.storage.StorageLevel;
+import org.json.JSONObject;
+import org.apache.log4j.Logger;
+import org.apache.log4j.Level;
+
+public class Runner2 {
+	private static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(Runner.class);
+
+	public static void main(String[] args) throws Exception {
+
+		try {
+
+			SparkSession spark = SparkSession.builder().appName("RedisStreamExample")
+					.config("spark.redis.host", "**.*.*.***").config("spark.redis.port", "6379").getOrCreate();		
+
+			StructType UserSchema5 = new StructType(new StructField[] {
+
+					DataTypes.createStructField("make", DataTypes.StringType, false),
+					DataTypes.createStructField("year", DataTypes.IntegerType, false),
+					DataTypes.createStructField("serialnum", DataTypes.IntegerType, false),
+					DataTypes.createStructField("model", DataTypes.StringType, false) });
+
+			org.apache.spark.sql.catalyst.encoders.ExpressionEncoder<org.apache.spark.sql.Row> enc5 = org.apache.spark.sql.catalyst.encoders.RowEncoder
+					.apply(UserSchema5);
+
+			Dataset<Row> RedisData = 
+					spark.readStream()
+					.format("redis")
+					.option("stream.keys", "carsstream")
+					.schema(UserSchema5).load();
+
+			
+			RedisData.show();
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
